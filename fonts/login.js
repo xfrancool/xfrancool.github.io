20dd63046b2975e2c5350a81a213de0b6fc92210
xfrancool <francoortega961@gmail.com>
2022-10-15 23:40:45 -0300

    c7

--- fonts/login.js
+++ fonts/login.js
@@ -7,14 +7,14 @@ function login() {
 
   if (user == "Sherleck" && pass == "1516") {
     window.location = "grupos/primero/grupo1.html";
+  } else if (user == "Hernan" && pass == "kant187im") {
+    window.location = "grupos/hernan/hernan.html";
   } else if (user == "Joshua" && pass == "1517") {
     window.location = "grupos/primeroA/grupoA.html";
   } else if (user == "Sergio29m" && pass == "5445") {
     window.location = "grupos/sergio/sergio.html";
   } else if (user == "EricLista" && pass == "1518") {
     window.location = "grupos/eric/eric.html";
-  } else if (user == "Hernann" && pass == "kant187im") {
-    window.location = "grupos/hernan/hernan.html";
   } else if (user == "Mateo1" && pass == "1519") {
     window.location = "grupos/mateo/mateo.html";
   } else if (user == "elimaryarg" && pass == "hola12") {
