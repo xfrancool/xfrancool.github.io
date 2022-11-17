ad77324c277a309260ec8ba3893512d76fc9a6d6
xfrancool <francoortega961@gmail.com>
2022-11-17 00:42:36 -0300

    c1

---
+++ fonts/log.js
@@ -0,0 +1,16 @@
+function login() {
+  var user, pass;
+
+  user = document.getElementById("usuario").value;
+  pass = document.getElementById("password").value;
+  btn = document.getElementById("loading");
+
+  if (user == "xfrancool" && pass == "230596") {
+    window.location = "assets/inicio.html";
+  } else {
+    alert("Por favor, ingrese usuario y contraseña corectos");
+
+    btn.classList.remove("class1");
+    btn.classList.add("class2");
+  }
+}
