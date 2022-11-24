cfb12470d88ae9b714412cc357d4994e34234412
xfrancool <francoortega961@gmail.com>
2022-11-24 00:46:40 -0300

    c1

--- fonts/log.js
+++ fonts/log.js
@@ -7,7 +7,7 @@ function login() {
 
   if (user == "xfrancool" && pass == "230596") {
     window.location = "assets/inicio.html";
-  } else if (user == "prueba" && pass == "1234") {
+  } else if (user == "invitado" && pass == "capa2022") {
     window.location = "assets/user/prueba/prueba.html";
   } else {
     alert("Por favor, ingrese usuario y contraseña corectos");
