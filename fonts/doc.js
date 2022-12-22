d55512895c1682556b594dcbf401358559910c78
xfrancool <francoortega961@gmail.com>
2022-12-22 15:52:49 -0300

    c1

---
+++ fonts/doc.js
@@ -0,0 +1,20 @@
+function login() {
+  var user, pass;
+
+  user = document.getElementById("usuario").value;
+  pass = document.getElementById("password").value;
+  btn = document.getElementById("loading");
+
+  if (user == "xfrancool" && pass == "230596") {
+    window.location = "assets/inicio.html";
+  } else if (user == "elimaryarg" && pass == "hola12") {
+    window.location = "assets/user/invitado/invitado.html";
+  } else if (user == "invitado" && pass == "capa2022") {
+    window.location = "assets/user/invitado/invitado.html";
+  } else {
+    alert("Por favor, ingrese usuario y contraseña corectos");
+
+    btn.classList.remove("class1");
+    btn.classList.add("class2");
+  }
+}
