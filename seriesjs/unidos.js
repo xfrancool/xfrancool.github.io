62e54f2bb27faab49b9d7c80619b25fcfa8bf53d
xfrancool <francoortega961@gmail.com>
2023-06-24 03:39:06 -0300

    c3

--- seriesjs/unidos.js
+++ seriesjs/unidos.js
@@ -173,7 +173,7 @@ function _0x1cb8() {
     "link",
     "6859643dmsWdL",
     "2649500ZxswFF",
-    "hhttps://api.npoint.io/a18367467decc429f92e",
+    "https://api.npoint.io/a18367467decc429f92e",
   ];
   _0x1cb8 = function () {
     return _0x5b1351;
