f1c251687f1a20c8adb83940cc1d11ba1e0b5fc2
xfrancool <francoortega961@gmail.com>
2023-09-11 03:36:07 -0300

    c3

--- unctorneo/plantilla.js
+++ unctorneo/plantilla.js
@@ -1,102 +1,150 @@
-// Obtén una referencia al elemento input del alias
-const aliasInput = document.querySelector(".copy input.text");
-
-// Obtén una referencia al elemento de WhatsApp
-const whatsappLink = document.querySelector(".social-button.whatsapp");
-
-// Obtén una referencia al elemento de web
-const webLink = document.querySelector("#web");
-
-// Obtén una referencia al elemento de facebook
-const facebookLink = document.querySelector("#facebook");
-
-// Obtén una referencia al elemento de Instagram
-const instagramLink = document.querySelector("#instagram");
-
-// Resto de tu código
-// ...
-
-let copyText = document.querySelector(".copy");
-let button = copyText.querySelector("button");
-button.addEventListener("click", function () {
-  let input = copyText.querySelector("input.text");
-  input.select();
-  document.execCommand("copy");
-  copyText.classList.add("active");
-  window.getSelection().removeAllRanges();
-  setTimeout(function () {
-    copyText.classList.remove("active");
-  }, 2500); // Corrija la sintaxis aquí
-});
-
-// Obtén una referencia al elemento <p> donde deseas mostrar el aviso.
-const avisoElement = document.getElementById("aviso");
-
-// URL del JSON externo
-const url = "https://api.npoint.io/9e0851ac36cbcc210919";
-
-// Realiza la solicitud fetch al JSON externo
-fetch(url)
-  .then((response) => {
-    if (!response.ok) {
-      throw new Error("Network response was not ok");
+const _0x3f611c = _0x5467;
+(function (_0x3c91ab, _0x5363d7) {
+  const _0x4dc674 = _0x5467,
+    _0x49c01a = _0x3c91ab();
+  while (!![]) {
+    try {
+      const _0x25531f =
+        parseInt(_0x4dc674(0x7b)) / 0x1 +
+        parseInt(_0x4dc674(0x6f)) / 0x2 +
+        -parseInt(_0x4dc674(0x6c)) / 0x3 +
+        (parseInt(_0x4dc674(0x88)) / 0x4) * (-parseInt(_0x4dc674(0x75)) / 0x5) +
+        parseInt(_0x4dc674(0x86)) / 0x6 +
+        parseInt(_0x4dc674(0x7d)) / 0x7 +
+        (-parseInt(_0x4dc674(0x7a)) / 0x8) * (parseInt(_0x4dc674(0x84)) / 0x9);
+      if (_0x25531f === _0x5363d7) break;
+      else _0x49c01a["push"](_0x49c01a["shift"]());
+    } catch (_0x5f47a1) {
+      _0x49c01a["push"](_0x49c01a["shift"]());
     }
-    return response.json(); // Parsea la respuesta JSON
+  }
+})(_0x3153, 0x97f26);
+function _0x5467(_0x286c5d, _0x158bcd) {
+  const _0x31537c = _0x3153();
+  return (
+    (_0x5467 = function (_0x546732, _0x167a38) {
+      _0x546732 = _0x546732 - 0x69;
+      let _0x421d71 = _0x31537c[_0x546732];
+      return _0x421d71;
+    }),
+    _0x5467(_0x286c5d, _0x158bcd)
+  );
+}
+const aliasInput = document[_0x3f611c(0x87)](_0x3f611c(0x8d)),
+  whatsappLink = document[_0x3f611c(0x87)](_0x3f611c(0x81)),
+  webLink = document["querySelector"](_0x3f611c(0x72)),
+  facebookLink = document[_0x3f611c(0x87)](_0x3f611c(0x82)),
+  instagramLink = document[_0x3f611c(0x87)](_0x3f611c(0x7f));
+let copyText = document[_0x3f611c(0x87)](".copy"),
+  button = copyText[_0x3f611c(0x87)]("button");
+button["addEventListener"](_0x3f611c(0x89), function () {
+  const _0x2e8056 = _0x3f611c;
+  let _0x56c16e = copyText[_0x2e8056(0x87)](_0x2e8056(0x78));
+  _0x56c16e[_0x2e8056(0x7e)](),
+    document["execCommand"]("copy"),
+    copyText[_0x2e8056(0x73)][_0x2e8056(0x6b)](_0x2e8056(0x8b)),
+    window[_0x2e8056(0x83)]()[_0x2e8056(0x85)](),
+    setTimeout(function () {
+      const _0x465c0f = _0x2e8056;
+      copyText["classList"]["remove"](_0x465c0f(0x8b));
+    }, 0x9c4);
+});
+const avisoElement = document[_0x3f611c(0x80)](_0x3f611c(0x76)),
+  url = _0x3f611c(0x6d);
+function _0x3153() {
+  const _0x2ae699 = [
+    "removeAllRanges",
+    "3630348uxlEuK",
+    "querySelector",
+    "268KjPNtG",
+    "click",
+    "\x20</a>\x20a\x20las\x20",
+    "active",
+    "value",
+    ".copy\x20input.text",
+    "innerHTML",
+    "\x0a\x20\x20\x20\x20<h1>🔹\x20Información\x20sobre\x20el\x20Torneo</h1>\x0a\x20\x20\x20\x20<br\x20/>\x0a\x20\x20\x20\x20<p>⋄\x20El\x20Torneo\x20se\x20llevará\x20a\x20cabo\x20el\x20día\x20",
+    "error",
+    "add",
+    "909834jBGDqq",
+    "https://api.npoint.io/9e0851ac36cbcc210919",
+    ".\x20Las\x20transferencias\x20destino\x20es\x20a\x20",
+    "1664964VEPZbz",
+    "\x20la\x20noche\x20antes\x20del\x20evento\x20para\x20que\x20podamos\x20enviar\x20el\x20pareo\x20a\x20todos\x20los\x20participantes.📋</p>\x0a\x20\x20\x20\x20<br\x20/>\x0a\x20\x20\x0a\x20\x20\x20\x20<p>⋄\x20En\x20el\x20formulario,\x20puedes\x20adjuntar\x20el\x20comprobante\x20de\x20pago.\x20Si\x20no\x20puedes\x20enviarlo\x20a\x20través\x20del\x20formulario,\x20también\x20puedes\x20hacerlo\x20haciendo\x20clic\x20en\x20el\x20logo\x20de\x20WhatsApp.</p>\x0a\x20\x20\x20\x20<br\x20/>\x0a\x20\x20\x0a\x0a\x20\x20\x20\x20<p>⋄\x20El\x20precio\x20de\x20la\x20inscripción\x20es\x20de\x20",
+    "\x20en\x20la<a\x20href=\x22",
+    "#web",
+    "classList",
+    "href",
+    "44110xpkipt",
+    "aviso",
+    "json",
+    "input.text",
+    "Error\x20al\x20obtener\x20datos:",
+    "3400rkSwXG",
+    "841512ledATZ",
+    "then",
+    "1788843PARzVl",
+    "select",
+    "#instagram",
+    "getElementById",
+    ".social-button.whatsapp",
+    "#facebook",
+    "getSelection",
+    "21555GkUuyX",
+  ];
+  _0x3153 = function () {
+    return _0x2ae699;
+  };
+  return _0x3153();
+}
+fetch(url)
+  [_0x3f611c(0x7c)]((_0x48f8d9) => {
+    const _0x3e46dd = _0x3f611c;
+    if (!_0x48f8d9["ok"])
+      throw new Error("Network\x20response\x20was\x20not\x20ok");
+    return _0x48f8d9[_0x3e46dd(0x77)]();
   })
-  .then((data) => {
-    // Accede a los datos que deseas mostrar en el mensaje
-    const primerAviso = data[0];
-    const {
-      cierre,
-      hora,
-      alias,
-      precio,
-      fecha,
-      transferencias,
-      maps,
-      lugar,
-      wpplink,
-      facebook,
-      web,
-      instagram,
-    } = primerAviso;
-
-    // Cambia el href del enlace de WhatsApp
-    whatsappLink.href = wpplink + fecha + "!";
-
-    // Cambia el href del enlace de Instagram
-    instagramLink.href = instagram;
-
-    // Cambia el href del enlace de Facebook
-    facebookLink.href = facebook;
-
-    // Cambia el href del enlace de la web
-    webLink.href = web;
-
-    // Cambia el valor del input
-    aliasInput.value = alias;
-
-    // Crea un mensaje utilizando etiquetas HTML
-    const mensaje = `
-    <h1>🔹 Información sobre el Torneo</h1>
-    <br />
-    <p>⋄ El Torneo se llevará a cabo el día ${fecha} en la<a href="${maps}" Target="_blank"> ${lugar} </a> a las ${hora}</p>
-    <br />
-   
-    <p>⋄ Las inscripciones se cerrarán el ${cierre} la noche antes del evento para que podamos enviar el pareo a todos los participantes.📋</p>
-    <br />
-  
-    <p>⋄ En el formulario, puedes adjuntar el comprobante de pago. Si no puedes enviarlo a través del formulario, también puedes hacerlo haciendo clic en el logo de WhatsApp.</p>
-    <br />
-  
-
-    <p>⋄ El precio de la inscripción es de ${precio}. Las transferencias destino es a ${transferencias}.✔️</p>
-      
-    `;
-
-    // Actualiza el contenido del elemento con el mensaje que contiene etiquetas HTML
-    avisoElement.innerHTML = mensaje;
+  [_0x3f611c(0x7c)]((_0x501877) => {
+    const _0x1f63ce = _0x3f611c,
+      _0x317156 = _0x501877[0x0],
+      {
+        cierre: _0x5d5f77,
+        hora: _0x4df020,
+        alias: _0xa07fc8,
+        precio: _0x7d5dc6,
+        fecha: _0x21f642,
+        transferencias: _0x2ea050,
+        maps: _0xa0d311,
+        lugar: _0x22e224,
+        wpplink: _0xfe6ef8,
+        facebook: _0x57eb29,
+        web: _0x3293c8,
+        instagram: _0x139d24,
+      } = _0x317156;
+    (whatsappLink[_0x1f63ce(0x74)] = _0xfe6ef8 + _0x21f642 + "!"),
+      (instagramLink[_0x1f63ce(0x74)] = _0x139d24),
+      (facebookLink[_0x1f63ce(0x74)] = _0x57eb29),
+      (webLink[_0x1f63ce(0x74)] = _0x3293c8),
+      (aliasInput[_0x1f63ce(0x8c)] = _0xa07fc8);
+    const _0x59e25a =
+      _0x1f63ce(0x69) +
+      _0x21f642 +
+      _0x1f63ce(0x71) +
+      _0xa0d311 +
+      "\x22\x20Target=\x22_blank\x22>\x20" +
+      _0x22e224 +
+      _0x1f63ce(0x8a) +
+      _0x4df020 +
+      "</p>\x0a\x20\x20\x20\x20<br\x20/>\x0a\x20\x20\x20\x0a\x20\x20\x20\x20<p>⋄\x20Las\x20inscripciones\x20se\x20cerrarán\x20el\x20" +
+      _0x5d5f77 +
+      _0x1f63ce(0x70) +
+      _0x7d5dc6 +
+      _0x1f63ce(0x6e) +
+      _0x2ea050 +
+      ".✔️</p>\x0a\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20";
+    avisoElement[_0x1f63ce(0x8e)] = _0x59e25a;
   })
-  .catch((error) => {
-    console.error("Error al obtener datos:", error);
+  ["catch"]((_0x3522f8) => {
+    const _0x1d178a = _0x3f611c;
+    console[_0x1d178a(0x6a)](_0x1d178a(0x79), _0x3522f8);
   });
