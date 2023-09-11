c876ef85d6e05f08161c57b0d75186219cc9c6b9
xfrancool <francoortega961@gmail.com>
2023-09-10 21:35:57 -0300

    c3

---
+++ unctorneo/plantilla.js
@@ -0,0 +1,150 @@
+function _0x2685() {
+  const _0x4920cd = [
+    "12279157WXQDRr",
+    "button",
+    "click",
+    "addEventListener",
+    "5343882DpfvOS",
+    ".social-button.whatsapp",
+    ".\x20Las\x20transferencias\x20destino\x20es\x20a\x20",
+    "getSelection",
+    ".✔️</p>\x0a\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20",
+    "8108PNJRkI",
+    "1882278OCraPK",
+    "\x20en\x20la<a\x20href=\x22",
+    ".copy\x20input.text",
+    "execCommand",
+    "remove",
+    "href",
+    "1kgYiWT",
+    "error",
+    "active",
+    "17016NTDhLF",
+    "1151985UFHbGx",
+    "1477PcyKUG",
+    "querySelector",
+    "2815gCVEly",
+    "\x22\x20Target=\x22_blank\x22>\x20",
+    "select",
+    "classList",
+    "\x20</a>\x20a\x20las\x20",
+    "aviso",
+    "copy",
+    "input.text",
+    "getElementById",
+    "Network\x20response\x20was\x20not\x20ok",
+    "82557qUkFnF",
+    "\x20la\x20noche\x20antes\x20del\x20evento\x20para\x20que\x20podamos\x20enviar\x20el\x20pareo\x20a\x20todos\x20los\x20participantes.📋</p>\x0a\x20\x20\x20\x20<br\x20/>\x0a\x20\x20\x0a\x20\x20\x20\x20<p>⋄\x20En\x20el\x20formulario,\x20puedes\x20adjuntar\x20el\x20comprobante\x20de\x20pago.\x20Si\x20no\x20puedes\x20enviarlo\x20a\x20través\x20del\x20formulario,\x20también\x20puedes\x20hacerlo\x20haciendo\x20clic\x20en\x20el\x20logo\x20de\x20WhatsApp.</p>\x0a\x20\x20\x20\x20<br\x20/>\x0a\x20\x20\x0a\x20\x20\x20\x20<p>⋄\x20Si\x20prefieres\x20pagar\x20en\x20efectivo,\x20no\x20es\x20necesario\x20enviar\x20ningún\x20comprobante.✔️</p>\x0a\x20\x20\x20\x20<br\x20/>\x0a\x20\x20\x20\x20<p>⋄\x20El\x20precio\x20de\x20la\x20inscripción\x20es\x20de\x20",
+    "710dJSape",
+    "</p>\x0a\x20\x20\x20\x20<br\x20/>\x0a\x20\x20\x20\x0a\x20\x20\x20\x20<p>⋄\x20Las\x20inscripciones\x20se\x20cerrarán\x20el\x20",
+    "TIRA.CLASE.RUINA",
+    "then",
+    "json",
+    ".copy",
+    "removeAllRanges",
+  ];
+  _0x2685 = function () {
+    return _0x4920cd;
+  };
+  return _0x2685();
+}
+const _0x55ff71 = _0x16fe;
+(function (_0x56214b, _0x98f392) {
+  const _0x31b71b = _0x16fe,
+    _0xf8b514 = _0x56214b();
+  while (!![]) {
+    try {
+      const _0x5c9f32 =
+        (-parseInt(_0x31b71b(0x1d2)) / 0x1) *
+          (-parseInt(_0x31b71b(0x1cc)) / 0x2) +
+        parseInt(_0x31b71b(0x1d6)) / 0x3 +
+        (parseInt(_0x31b71b(0x1cb)) / 0x4) *
+          (-parseInt(_0x31b71b(0x1d9)) / 0x5) +
+        -parseInt(_0x31b71b(0x1c6)) / 0x6 +
+        (-parseInt(_0x31b71b(0x1d7)) / 0x7) *
+          (parseInt(_0x31b71b(0x1d5)) / 0x8) +
+        (parseInt(_0x31b71b(0x1e3)) / 0x9) *
+          (parseInt(_0x31b71b(0x1bb)) / 0xa) +
+        parseInt(_0x31b71b(0x1c2)) / 0xb;
+      if (_0x5c9f32 === _0x98f392) break;
+      else _0xf8b514["push"](_0xf8b514["shift"]());
+    } catch (_0x565006) {
+      _0xf8b514["push"](_0xf8b514["shift"]());
+    }
+  }
+})(_0x2685, 0x956db);
+const aliasInput = document[_0x55ff71(0x1d8)](_0x55ff71(0x1ce));
+aliasInput["value"] = _0x55ff71(0x1bd);
+const whatsappLink = document[_0x55ff71(0x1d8)](_0x55ff71(0x1c7));
+let copyText = document["querySelector"](_0x55ff71(0x1c0)),
+  button = copyText[_0x55ff71(0x1d8)](_0x55ff71(0x1c3));
+button[_0x55ff71(0x1c5)](_0x55ff71(0x1c4), function () {
+  const _0x3177a3 = _0x55ff71;
+  let _0x929f03 = copyText[_0x3177a3(0x1d8)](_0x3177a3(0x1e0));
+  _0x929f03[_0x3177a3(0x1db)](),
+    document[_0x3177a3(0x1cf)](_0x3177a3(0x1df)),
+    copyText[_0x3177a3(0x1dc)]["add"]("active"),
+    window[_0x3177a3(0x1c9)]()[_0x3177a3(0x1c1)](),
+    setTimeout(function () {
+      const _0x2f4af8 = _0x3177a3;
+      copyText[_0x2f4af8(0x1dc)][_0x2f4af8(0x1d0)](_0x2f4af8(0x1d4));
+    }, 0x9c4);
+});
+const avisoElement = document[_0x55ff71(0x1e1)](_0x55ff71(0x1de)),
+  url = "https://api.npoint.io/9e0851ac36cbcc210919";
+function _0x16fe(_0x35eafb, _0x22e4d6) {
+  const _0x268529 = _0x2685();
+  return (
+    (_0x16fe = function (_0x16fee5, _0x41f9db) {
+      _0x16fee5 = _0x16fee5 - 0x1bb;
+      let _0x5d5f58 = _0x268529[_0x16fee5];
+      return _0x5d5f58;
+    }),
+    _0x16fe(_0x35eafb, _0x22e4d6)
+  );
+}
+fetch(url)
+  [_0x55ff71(0x1be)]((_0x410e93) => {
+    const _0x38eebe = _0x55ff71;
+    if (!_0x410e93["ok"]) throw new Error(_0x38eebe(0x1e2));
+    return _0x410e93[_0x38eebe(0x1bf)]();
+  })
+  [_0x55ff71(0x1be)]((_0x9bfe22) => {
+    const _0x2c07fa = _0x55ff71,
+      _0x1a8020 = _0x9bfe22[0x0],
+      {
+        cierre: _0x235e1c,
+        hora: _0x5a9105,
+        alias: _0x2d8c12,
+        precio: _0x477647,
+        fecha: _0x47a1de,
+        encargado: _0x2e9f74,
+        maps: _0x288072,
+        facu: _0x3e4195,
+      } = _0x1a8020;
+    whatsappLink[_0x2c07fa(0x1d1)] =
+      "https://api.whatsapp.com/send?phone=+5493548500137&text=Hola,\x20entrego\x20el\x20comprobante\x20de\x20pago\x20del\x20torneo\x20del\x20día\x20" +
+      _0x47a1de +
+      "!";
+    const _0x27fabb =
+      "\x0a\x20\x20\x20\x20<h1>🔹\x20Información\x20sobre\x20el\x20Torneo</h1>\x0a\x20\x20\x20\x20<br\x20/>\x0a\x20\x20\x20\x20<p>⋄\x20El\x20Torneo\x20se\x20llevará\x20a\x20cabo\x20el\x20día\x20" +
+      _0x47a1de +
+      _0x2c07fa(0x1cd) +
+      _0x288072 +
+      _0x2c07fa(0x1da) +
+      _0x3e4195 +
+      _0x2c07fa(0x1dd) +
+      _0x5a9105 +
+      _0x2c07fa(0x1bc) +
+      _0x235e1c +
+      _0x2c07fa(0x1e4) +
+      _0x477647 +
+      _0x2c07fa(0x1c8) +
+      _0x2e9f74 +
+      _0x2c07fa(0x1ca);
+    avisoElement["innerHTML"] = _0x27fabb;
+  })
+  ["catch"]((_0x18b7e2) => {
+    const _0x11b3fa = _0x55ff71;
+    console[_0x11b3fa(0x1d3)]("Error\x20al\x20obtener\x20datos:", _0x18b7e2);
+  });
