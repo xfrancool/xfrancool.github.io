c560e64793d3c1233ea4c807c937c2cfb83f3d8a
xfrancool <francoortega961@gmail.com>
2023-10-03 04:18:40 -0300

    c3

---
+++ unctorneo/plantillav2.js
@@ -0,0 +1,184 @@
+const _0x4d48ef = _0x534e;
+(function (_0x14e0df, _0x544054) {
+  const _0x458eac = _0x534e,
+    _0x3f1d72 = _0x14e0df();
+  while (!![]) {
+    try {
+      const _0x42c8f5 =
+        (-parseInt(_0x458eac(0x1cb)) / 0x1) *
+          (-parseInt(_0x458eac(0x1ba)) / 0x2) +
+        -parseInt(_0x458eac(0x1ac)) / 0x3 +
+        parseInt(_0x458eac(0x1cf)) / 0x4 +
+        (parseInt(_0x458eac(0x1b4)) / 0x5) *
+          (-parseInt(_0x458eac(0x1a6)) / 0x6) +
+        (parseInt(_0x458eac(0x1b5)) / 0x7) *
+          (-parseInt(_0x458eac(0x1cd)) / 0x8) +
+        (parseInt(_0x458eac(0x1a3)) / 0x9) *
+          (-parseInt(_0x458eac(0x1be)) / 0xa) +
+        parseInt(_0x458eac(0x1cc)) / 0xb;
+      if (_0x42c8f5 === _0x544054) break;
+      else _0x3f1d72["push"](_0x3f1d72["shift"]());
+    } catch (_0x49e6e0) {
+      _0x3f1d72["push"](_0x3f1d72["shift"]());
+    }
+  }
+})(_0x76b4, 0x7ee63);
+function _0x534e(_0x19cb28, _0x368644) {
+  const _0x76b40b = _0x76b4();
+  return (
+    (_0x534e = function (_0x534e2a, _0x3e39c9) {
+      _0x534e2a = _0x534e2a - 0x1a0;
+      let _0xf72a5e = _0x76b40b[_0x534e2a];
+      return _0xf72a5e;
+    }),
+    _0x534e(_0x19cb28, _0x368644)
+  );
+}
+function _0x76b4() {
+  const _0x273b81 = [
+    "6eqZyOt",
+    "\x0a\x20\x20\x20\x20<h1>🔹\x20Información\x20sobre\x20el\x20Torneo</h1>\x0a\x20\x20\x20\x20<br\x20/>\x0a\x20\x20\x20\x20<p>⋄\x20",
+    ".✔️</p>\x0a\x20\x20\x20\x20\x20\x20\x0a\x20\x20\x20\x20",
+    "\x20en\x20la<a\x20href=\x22",
+    "aviso",
+    "add",
+    "2350293ktqdIS",
+    "https://api.npoint.io/9e0851ac36cbcc210919",
+    "value",
+    "click",
+    "innerHTML",
+    "\x20</a>\x20a\x20las\x20",
+    "setSelectionRange",
+    "#instagram",
+    "4051045BTPzIV",
+    "5779823YddhnF",
+    "button",
+    "querySelector",
+    "clipboard",
+    "catch",
+    "2MgFSKl",
+    "</p>\x0a\x20\x20\x20\x20<br\x20/>\x0a\x20\x20\x0a\x20\x20\x20\x20<p>⋄\x20",
+    ".copy\x20input.text",
+    "</p>\x0a\x20\x20\x20\x20<br\x20/>\x0a\x20\x20\x0a\x0a\x20\x20\x20\x20<p>⋄\x20El\x20precio\x20de\x20la\x20inscripción\x20es\x20de\x20",
+    "6040aFZzJs",
+    "input.text",
+    "</p>\x0a\x20\x20\x20\x20<br\x20/>\x0a\x20\x20\x20\x0a\x20\x20\x20\x20<p>⋄\x20",
+    "Error\x20al\x20obtener\x20datos:",
+    "href",
+    "remove",
+    "\x20</p>\x0a\x20\x20\x20\x20<br\x20/>\x0a\x20\x20\x20\x20<p>⋄\x20",
+    "No\x20se\x20pudo\x20copiar\x20al\x20portapapeles:\x20",
+    "#facebook",
+    "\x20</p>\x0a\x20\x20\x20\x20<br\x20/>\x0a\x20\x20\x20\x20<p>⋄\x20El\x20Torneo\x20se\x20llevará\x20a\x20cabo\x20el\x20día\x20",
+    "\x22\x20Target=\x22_blank\x22>\x20",
+    "active",
+    "classList",
+    "64585JULzql",
+    "27020598cSioLS",
+    "8nmcIun",
+    "then",
+    "3184836qJTFch",
+    ".copy",
+    "removeAllRanges",
+    "Network\x20response\x20was\x20not\x20ok",
+    "5634XSvAzJ",
+    "writeText",
+    ".social-button.whatsapp",
+  ];
+  _0x76b4 = function () {
+    return _0x273b81;
+  };
+  return _0x76b4();
+}
+const aliasInput = document["querySelector"](_0x4d48ef(0x1bc)),
+  whatsappLink = document[_0x4d48ef(0x1b7)](_0x4d48ef(0x1a5)),
+  webLink = document[_0x4d48ef(0x1b7)]("#web"),
+  facebookLink = document["querySelector"](_0x4d48ef(0x1c6)),
+  instagramLink = document[_0x4d48ef(0x1b7)](_0x4d48ef(0x1b3));
+let copyText = document["querySelector"](_0x4d48ef(0x1a0)),
+  button = copyText["querySelector"](_0x4d48ef(0x1b6)),
+  input = copyText["querySelector"](_0x4d48ef(0x1bf));
+button["addEventListener"](_0x4d48ef(0x1af), function () {
+  const _0x9ef67a = _0x4d48ef;
+  input["select"](),
+    input[_0x9ef67a(0x1b2)](0x0, 0x1869f),
+    navigator[_0x9ef67a(0x1b8)]
+      [_0x9ef67a(0x1a4)](input["value"])
+      [_0x9ef67a(0x1ce)](function () {
+        const _0xb4507d = _0x9ef67a;
+        copyText["classList"][_0xb4507d(0x1ab)]("active"),
+          window["getSelection"]()[_0xb4507d(0x1a1)](),
+          setTimeout(function () {
+            const _0xde8a5b = _0xb4507d;
+            copyText[_0xde8a5b(0x1ca)][_0xde8a5b(0x1c3)](_0xde8a5b(0x1c9));
+          }, 0x9c4);
+      })
+      [_0x9ef67a(0x1b9)](function (_0xbf0fdd) {
+        const _0x125fec = _0x9ef67a;
+        console["error"](_0x125fec(0x1c5), _0xbf0fdd);
+      });
+});
+const avisoElement = document["getElementById"](_0x4d48ef(0x1aa)),
+  url = _0x4d48ef(0x1ad);
+fetch(url)
+  [_0x4d48ef(0x1ce)]((_0x325bbc) => {
+    const _0x515562 = _0x4d48ef;
+    if (!_0x325bbc["ok"]) throw new Error(_0x515562(0x1a2));
+    return _0x325bbc["json"]();
+  })
+  [_0x4d48ef(0x1ce)]((_0x5d275c) => {
+    const _0x5a452e = _0x4d48ef,
+      _0x5c5c5c = _0x5d275c[0x0],
+      {
+        cierre: _0x2c2ba5,
+        hora: _0x3ded4c,
+        alias: _0x931eaf,
+        precio: _0x1fac06,
+        fecha: _0x22f2ee,
+        transferencias: _0x46c6f6,
+        maps: _0x4e351e,
+        lugar: _0xeb2498,
+        wpplink: _0x326a89,
+        facebook: _0x542a3d,
+        web: _0x34ca69,
+        instagram: _0x4d7056,
+        ritmo: _0x57fca3,
+        catego: _0x29f6b6,
+        premios: _0x5cac5b,
+        adjuntar: _0x363543,
+      } = _0x5c5c5c;
+    (whatsappLink[_0x5a452e(0x1c2)] = _0x326a89 + _0x22f2ee + "!"),
+      (instagramLink[_0x5a452e(0x1c2)] = _0x4d7056),
+      (facebookLink[_0x5a452e(0x1c2)] = _0x542a3d),
+      (webLink["href"] = _0x34ca69),
+      (aliasInput[_0x5a452e(0x1ae)] = _0x931eaf);
+    const _0x36d553 =
+      _0x5a452e(0x1a7) +
+      _0x57fca3 +
+      _0x5a452e(0x1c4) +
+      _0x29f6b6 +
+      _0x5a452e(0x1c4) +
+      _0x5cac5b +
+      _0x5a452e(0x1c7) +
+      _0x22f2ee +
+      _0x5a452e(0x1a9) +
+      _0x4e351e +
+      _0x5a452e(0x1c8) +
+      _0xeb2498 +
+      _0x5a452e(0x1b1) +
+      _0x3ded4c +
+      _0x5a452e(0x1c0) +
+      _0x2c2ba5 +
+      _0x5a452e(0x1bb) +
+      _0x363543 +
+      _0x5a452e(0x1bd) +
+      _0x1fac06 +
+      ".\x20Las\x20transferencias\x20destino\x20es\x20a\x20" +
+      _0x46c6f6 +
+      _0x5a452e(0x1a8);
+    avisoElement[_0x5a452e(0x1b0)] = _0x36d553;
+  })
+  ["catch"]((_0x1343a5) => {
+    const _0x399271 = _0x4d48ef;
+    console["error"](_0x399271(0x1c1), _0x1343a5);
+  });
