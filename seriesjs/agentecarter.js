373dfb0e9721bd1a50bb40bfb5c21429e522fb47
xfrancool <francoortega961@gmail.com>
2023-12-29 10:41:25 -0300

    c3

--- seriesjs/agentecarter.js
+++ seriesjs/agentecarter.js
@@ -1,182 +1,129 @@
-function _0x4fa9(_0x394d43, _0x3693bb) {
-  const _0x1cb8a1 = _0x1cb8();
-  return (
-    (_0x4fa9 = function (_0x4fa9a1, _0x429571) {
-      _0x4fa9a1 = _0x4fa9a1 - 0x9d;
-      let _0x170452 = _0x1cb8a1[_0x4fa9a1];
-      return _0x170452;
-    }),
-    _0x4fa9(_0x394d43, _0x3693bb)
-  );
-}
-const _0x4982f9 = _0x4fa9;
-(function (_0x45d7f6, _0x2fcba2) {
-  const _0x50bc0f = _0x4fa9,
-    _0x5f4cc9 = _0x45d7f6();
-  while (!![]) {
-    try {
-      const _0x4fcb4d =
-        (-parseInt(_0x50bc0f(0xc8)) / 0x1) * (parseInt(_0x50bc0f(0xa0)) / 0x2) +
-        -parseInt(_0x50bc0f(0xae)) / 0x3 +
-        parseInt(_0x50bc0f(0xb4)) / 0x4 +
-        (parseInt(_0x50bc0f(0xaf)) / 0x5) * (-parseInt(_0x50bc0f(0xc0)) / 0x6) +
-        (-parseInt(_0x50bc0f(0xb3)) / 0x7) * (parseInt(_0x50bc0f(0xc1)) / 0x8) +
-        (-parseInt(_0x50bc0f(0xa4)) / 0x9) *
-          (-parseInt(_0x50bc0f(0xa2)) / 0xa) +
-        parseInt(_0x50bc0f(0xac)) / 0xb;
-      if (_0x4fcb4d === _0x2fcba2) break;
-      else _0x5f4cc9["push"](_0x5f4cc9["shift"]());
-    } catch (_0x328b41) {
-      _0x5f4cc9["push"](_0x5f4cc9["shift"]());
-    }
+import { series } from "./export.js";
+
+const globalName = "agentecarter";
+
+// Fetch series data
+fetch(series)
+  .then((response) => response.json())
+  .then((data) => {
+    const defaultName = globalName; // Change the value as needed
+    const defaultItem = data.find((item) => item.json === defaultName);
+
+    // Update HTML with default series image
+    updateHTML(defaultItem);
+  });
+
+// Function to update HTML with series image
+function updateHTML(selectedItem) {
+  if (selectedItem) {
+    const { img } = selectedItem;
+
+    // Update the image
+    const logoElement = document.getElementById("logo");
+    logoElement.src = img;
+
+    // Rest of your code...
   }
-})(_0x1cb8, 0xb73ad),
-  fetch(_0x4982f9(0xb5))
-    [_0x4982f9(0xa5)]((_0xecc988) => _0xecc988[_0x4982f9(0xc3)]())
-    [_0x4982f9(0xa5)]((_0x2f7f59) => {
-      const _0x574d14 = _0x4982f9,
-        _0x4fa540 = document[_0x574d14(0xaa)](_0x574d14(0xc9)),
-        _0x254cf8 = document["getElementById"](_0x574d14(0xa1)),
-        _0x48616a = document["getElementById"](_0x574d14(0xc4)),
-        _0x4029b1 = _0x574d14(0xb6);
-      for (
-        let _0x5ded57 = 0x0;
-        _0x5ded57 < _0x2f7f59[_0x574d14(0x9d)];
-        _0x5ded57 += 0x2
-      ) {
-        const _0x39a81e = _0x2f7f59[_0x5ded57],
-          _0x3006d2 = _0x2f7f59[_0x5ded57 + 0x1],
-          _0x3f1692 = document[_0x574d14(0xc2)](_0x574d14(0xa6));
-        (_0x3f1692["value"] = _0x39a81e),
-          (_0x3f1692[_0x574d14(0xba)] = _0x39a81e),
-          _0x4fa540[_0x574d14(0xb8)](_0x3f1692);
-      }
-      _0x4fa540[_0x574d14(0xc6)](_0x574d14(0xb7), (_0x36b7b2) => {
-        const _0x54fe7d = _0x574d14,
-          _0x23fd12 = _0x36b7b2[_0x54fe7d(0xad)][_0x54fe7d(0xb1)];
-        (_0x254cf8[_0x54fe7d(0xbd)] = ""), (_0x48616a[_0x54fe7d(0xbd)] = "");
-        for (
-          let _0x414a64 = 0x0;
-          _0x414a64 < _0x2f7f59[_0x54fe7d(0x9d)];
-          _0x414a64 += 0x2
-        ) {
-          if (_0x2f7f59[_0x414a64] === _0x23fd12) {
-            const _0x1e1a73 = _0x2f7f59[_0x414a64 + 0x1];
-            for (
-              let _0x3f506c = 0x0;
-              _0x3f506c < _0x1e1a73[_0x54fe7d(0x9d)];
-              _0x3f506c++
-            ) {
-              const _0x266b74 = _0x1e1a73[_0x3f506c],
-                _0x4fb576 = document[_0x54fe7d(0xc2)]("li"),
-                _0x339667 = document[_0x54fe7d(0xc2)]("a");
-              (_0x339667[_0x54fe7d(0xbb)] = _0x266b74[_0x54fe7d(0xb2)]),
-                (_0x339667[_0x54fe7d(0xba)] = _0x266b74[_0x54fe7d(0xa7)]),
-                _0x4fb576[_0x54fe7d(0xb8)](_0x339667),
-                _0x254cf8[_0x54fe7d(0xb8)](_0x4fb576),
-                (_0x254cf8[_0x54fe7d(0x9e)][_0x54fe7d(0xab)] = _0x54fe7d(0xc7));
-              const _0x36a270 = _0x4bccca(_0x4029b1);
-              _0x36a270[_0x23fd12] &&
-                _0x36a270[_0x23fd12][_0x266b74[_0x54fe7d(0xa7)]] &&
-                (_0x339667["style"][_0x54fe7d(0xb9)] = _0x54fe7d(0xbf));
-            }
-            break;
-          }
-        }
-      });
-      function _0x4bccca(_0x268713) {
-        const _0x2bbb6f = _0x574d14,
-          _0x5f1bc8 = localStorage[_0x2bbb6f(0xa9)](
-            _0x2bbb6f(0xbe) + _0x268713
-          );
-        if (_0x5f1bc8) return JSON[_0x2bbb6f(0x9f)](_0x5f1bc8);
-        return {};
-      }
-      function _0x2cbf73(_0x30c80f, _0x27300f, _0x97ccaf) {
-        const _0x2b7bef = _0x574d14,
-          _0x4025c4 = _0x4bccca(_0x97ccaf);
-        !_0x4025c4[_0x27300f] && (_0x4025c4[_0x27300f] = {}),
-          (_0x4025c4[_0x27300f][_0x30c80f] = !![]),
-          localStorage[_0x2b7bef(0xb0)](
-            _0x2b7bef(0xbe) + _0x97ccaf,
-            JSON["stringify"](_0x4025c4)
-          ),
-          _0x973e58(_0x97ccaf);
-      }
-      function _0x973e58(_0x3182b3) {
-        const _0x2a757c = _0x574d14,
-          _0x4d3c9d = _0x4bccca(_0x3182b3),
-          _0x39d7cd = document[_0x2a757c(0xa3)](_0x2a757c(0xa8));
-        _0x39d7cd["forEach"]((_0x89ecba) => {
-          const _0x6a822a = _0x2a757c,
-            _0x183717 = _0x89ecba[_0x6a822a(0xba)],
-            _0x2eebb7 = _0x4fa540[_0x6a822a(0xb1)];
-          _0x4d3c9d[_0x2eebb7] && _0x4d3c9d[_0x2eebb7][_0x183717]
-            ? (_0x89ecba["style"][_0x6a822a(0xb9)] = "teal")
-            : (_0x89ecba[_0x6a822a(0x9e)]["color"] = "");
-        });
-      }
-      _0x254cf8["addEventListener"](_0x574d14(0xc5), (_0x25c725) => {
-        const _0x403d18 = _0x574d14;
-        if (_0x25c725[_0x403d18(0xad)][_0x403d18(0xbc)] === "A") {
-          _0x25c725["preventDefault"](),
-            showModal(_0x25c725["target"][_0x403d18(0xbb)]);
-          const _0x19fa45 = _0x25c725["target"][_0x403d18(0xba)],
-            _0x5a11c0 = _0x4fa540[_0x403d18(0xb1)];
-          _0x2cbf73(_0x19fa45, _0x5a11c0, _0x4029b1);
-        }
-      });
-    });
-function _0x1cb8() {
-  const _0x5b1351 = [
-    "agentecarter",
-    "change",
-    "appendChild",
-    "color",
-    "textContent",
-    "href",
-    "tagName",
-    "innerHTML",
-    "watchedCaps-",
-    "teal",
-    "60036YEDVDx",
-    "8tDcHIz",
-    "createElement",
-    "json",
-    "video-container",
-    "click",
-    "addEventListener",
-    "block",
-    "237Ogmqun",
-    "temp-select",
-    "length",
-    "style",
-    "parse",
-    "10446LCjglt",
-    "cap-list",
-    "1915010WuAkzJ",
-    "querySelectorAll",
-    "9UcQufC",
-    "then",
-    "option",
-    "cap",
-    "#cap-list\x20a",
-    "getItem",
-    "getElementById",
-    "display",
-    "39792170mlXQjc",
-    "target",
-    "486699ErgtVs",
-    "670ROGMml",
-    "setItem",
-    "value",
-    "link",
-    "6859643dmsWdL",
-    "2649500ZxswFF",
-    "https://xfrancool.github.io/seriesjson/agentecarter.json",
-  ];
-  _0x1cb8 = function () {
-    return _0x5b1351;
-  };
-  return _0x1cb8();
 }
+
+const url = "https://xfrancool.github.io/seriesjson/"; // Identificador único para la serie
+const type = ".json";
+
+const unificacion = url + globalName + type;
+
+fetch(unificacion)
+  .then((response) => response.json())
+  .then((data) => {
+    const select = document.getElementById("temp-select");
+    const capList = document.getElementById("cap-list");
+    const videoContainer = document.getElementById("video-container");
+    const id = globalName; // Identificador único para la serie
+
+    // Populate select with seasons
+    for (let i = 0; i < data.length; i += 2) {
+      const seasonName = data[i];
+      const seasonCaps = data[i + 1];
+      const option = document.createElement("option");
+      option.value = seasonName;
+      option.textContent = seasonName;
+      select.appendChild(option);
+    }
+
+    // Update capList when select changes
+    select.addEventListener("change", (e) => {
+      const selectedSeason = e.target.value;
+      capList.innerHTML = "";
+      videoContainer.innerHTML = "";
+
+      for (let i = 0; i < data.length; i += 2) {
+        if (data[i] === selectedSeason) {
+          const seasonCaps = data[i + 1];
+
+          for (let j = 0; j < seasonCaps.length; j++) {
+            const cap = seasonCaps[j];
+            const listItem = document.createElement("li");
+            const link = document.createElement("a");
+            link.href = cap.link;
+            link.textContent = cap.cap;
+            listItem.appendChild(link);
+            capList.appendChild(listItem);
+            capList.style.display = "block";
+
+            // Paint watched caps
+            const watched = getWatchedCaps(id);
+            if (watched[selectedSeason] && watched[selectedSeason][cap.cap]) {
+              link.style.color = "teal";
+            }
+          }
+
+          break;
+        }
+      }
+    });
+
+    // Get watched caps from Local Storage
+    function getWatchedCaps(id) {
+      const watched = localStorage.getItem(`watchedCaps-${id}`);
+      if (watched) {
+        return JSON.parse(watched);
+      }
+      return {};
+    }
+
+    // Mark cap as watched and save to Local Storage
+    function markAsWatched(cap, season, id) {
+      const watched = getWatchedCaps(id);
+      if (!watched[season]) {
+        watched[season] = {};
+      }
+      watched[season][cap] = true;
+      localStorage.setItem(`watchedCaps-${id}`, JSON.stringify(watched));
+      paintWatchedCaps(id);
+    }
+
+    // Paint watched caps
+    function paintWatchedCaps(id) {
+      const watched = getWatchedCaps(id);
+      const caps = document.querySelectorAll("#cap-list a");
+      caps.forEach((cap) => {
+        const capName = cap.textContent;
+        const seasonName = select.value;
+        if (watched[seasonName] && watched[seasonName][capName]) {
+          cap.style.color = "teal";
+        } else {
+          cap.style.color = "";
+        }
+      });
+    }
+
+    // Listen for click events on capList
+    capList.addEventListener("click", (event) => {
+      if (event.target.tagName === "A") {
+        event.preventDefault();
+        showModal(event.target.href);
+        const capName = event.target.textContent;
+        const seasonName = select.value;
+        markAsWatched(capName, seasonName, id);
+      }
+    });
+  });
